0b0155aa6ac8d17fcacd5fea5cc21d0609e9b370
zhuravlevma <monge.gaspard.1746@gmail.com>
2022-01-11 01:53:10 +0300

    add hashset

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::thread;
 use std::time::Duration;
 
@@ -8,20 +9,50 @@ fn main() {
     create_workout(intensity_user, random_number);
 }
 
+struct Cache<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    values: HashSet<u32>,
+}
+
+impl<T> Cache<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cache<T> {
+        Cache {
+            calculation,
+            values: HashSet::new(),
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        for value in &self.values {
+            if *value == arg {
+                return arg;
+            }
+        }
+        let value = (self.calculation)(arg);
+        self.values.insert(value);
+        value
+    }
+}
+
 fn create_workout(intensity: u32, random_number: u32) {
-    let closure = |num| {
+    let mut cache = Cache::new(|num| {
         println!("loading... slowly...");
         thread::sleep(Duration::from_secs(2));
         num
-    };
+    });
     if intensity < 25 {
-        println!("do {} push-ups", closure(intensity));
-        println!("Next, do {} squats", closure(intensity));
+        println!("do {} push-ups", cache.value(intensity));
+        println!("Next, do {} squats", cache.value(intensity));
+        println!("Next, do {} pull-ups", cache.value(22));
+    } else if random_number == 3 {
+        println!("take a break today");
     } else {
-        if random_number == 3 {
-            println!("take a break today");
-        } else {
-            println!("Today you have jogging {} minute", closure(intensity));
-        }
+        println!("Today you have jogging {} minute", cache.value(intensity));
     }
 }
